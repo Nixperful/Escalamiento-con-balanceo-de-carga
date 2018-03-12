1cdee7932f1e8ecd6a311b3d14048dd7f0e307b1
n.osorio <nicolas.osorio@mail.escuelaing.edu.co>
2018-03-12 15:01:42 -0500

    PARTE 1

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -33,13 +33,13 @@ var app = (function () {
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
         
-        //subscribe to /topic/TOPICXX when connections succeed
+        //subscribe to /topic/newpoint when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/TOPICXX', function (eventbody) {
-                
-                
+            stompClient.subscribe('/topic/newpoint', function (eventbody) {
+                alert(JSON.parse(eventbody.body).content);
             });
+            
         });
 
     };
@@ -61,6 +61,7 @@ var app = (function () {
             addPointToCanvas(pt);
 
             //publicar el evento
+            stompClient.send("/topic/newpoint", {}, JSON.stringify(pt)); 
         },
 
         disconnect: function () {
